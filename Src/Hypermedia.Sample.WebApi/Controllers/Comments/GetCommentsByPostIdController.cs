7087a351736623d32c73394db2180e77561a4c0f
Cain O'Sullivan <cain.osullivan@gmail.com>
2017-09-07 19:36:55 +0800

    fixed route

--- Src/Hypermedia.Sample.WebApi/Controllers/Comments/GetCommentsByPostIdController.cs
+++ Src/Hypermedia.Sample.WebApi/Controllers/Comments/GetCommentsByPostIdController.cs
@@ -12,17 +12,17 @@ namespace Hypermedia.Sample.WebApi.Controllers.Comments
         /// <summary>
         /// Returns the comments that are assigned to a post.
         /// </summary>
-        /// <param name="id">the ID of the post to return the comments for.</param>
+        /// <param name="postId">the ID of the post to return the comments for.</param>
         /// <param name="requestMetadata">The request metadata.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>The HTTP action result that represents the result of the action.</returns>
         [HttpGet, Route("v1/posts/{postId}/comments")]
         public async Task<IHttpActionResult> ExecuteAsync(
-            int id,
+            int postId,
             IRequestMetadata<CommentResource> requestMetadata = null,
             CancellationToken cancellationToken = default(CancellationToken))
         {
-            var post = await Database.Posts.GetByIdAsync(id, cancellationToken);
+            var post = await Database.Posts.GetByIdAsync(postId, cancellationToken);
 
             if (post == null)
             {
